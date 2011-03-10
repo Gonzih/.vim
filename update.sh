4f3939b5a1ff1f1049669e191ebc7d0cf71aea25
Max Gonzih <gonzih@gmail.com>
2011-03-10 15:36:33 +0200

    set branch to update script

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 git pull
 git submodule init
 git submodule update
-git submodule foreach git pull
+git submodule foreach git pull origin master
 git submodule foreach git checkout master
 git ci -a -m 'Update submodules to latest versions'
 git push
