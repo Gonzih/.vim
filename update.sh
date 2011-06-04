ae0029ea909351bb4c1c202992eca5cf043df307
Max Gonzih <gonzih@gmail.com>
2011-06-04 10:54:55 +0300

    remove update script now use vundle for all plugins

--- update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git pull
-git submodule init
-git submodule update
-git submodule foreach git pull origin master
-git submodule foreach git checkout master
-git ci -a -m 'Updated vundle to latest version'
-git push
