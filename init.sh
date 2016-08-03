44873bde93f39512b2f8793789890b741531aded
Max Gonzih <gonzih@gmail.com>
2016-08-03 19:17:10 +0200

    Add all go packages to the init script

--- init.sh
+++ init.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-ln -s ~/.vim/vimrc ~/.vimrc
-ln -s ~/.vim/vimrc ~/.ideavimrc
-ln -s ~/.vim/gvimrc ~/.gvimrc
-ln -s ~/.vim/gvimrc ~/.nvimrc
+set -e
+
+ln -sf ~/.vim/vimrc ~/.vimrc
+ln -sf ~/.vim/vimrc ~/.ideavimrc
+ln -sf ~/.vim/gvimrc ~/.gvimrc
+ln -sf ~/.vim/gvimrc ~/.nvimrc
+
+go get -u github.com/nsf/gocode
+go get -u golang.org/x/tools/cmd/goimports
+go get -u github.com/golang/lint/golint
+go get -u github.com/kisielk/errcheck
